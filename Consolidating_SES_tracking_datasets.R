100288b2f73beaae6053e6eeb1bea3d4b7938a9f
greendb <davo.b.green@gmail.com>
2022-05-16 17:06:19 +1000

    Adding relative file paths up front. Correcting incorrect case for pulling diag and dive data from ACCESS databases

--- Consolidating_SES_tracking_datasets.R
+++ Consolidating_SES_tracking_datasets.R
@@ -6,14 +6,16 @@ library(RODBC)
 library(fs)
 library(stringr)
 library(tidyselect)
-library(readr)
-library(here)
+
 # ----------------------------------------------------------------------------------------------------------------------------------- #
 
 # Get relative file paths for 
-
-p <- "c:/Users/greendb/OneDrive - University of Tasmania/SMRU Data/" # directory with zipped files
-df <- "ACCESS_Files" # Destination for unzipped files
+here::i_am(path = "./Consolidating_SES_tracking_datasets.R") # set root directory for project
+# p <- "c:/Users/greendb/OneDrive - University of Tasmania/SMRU Data/" # directory with zipped files
+p <- here::here("")
+# df <- "ACCESS_Files" # Destination for unzipped files
+df <- here::here("ACCESS_Files")
+fs::dir_create(path = here::here("Consolidated_datasets"))
 
 # ----------------------------------------------------------------------------------------------------------------------------------- #
 
@@ -26,7 +28,7 @@ flz <- dir_ls(path = p,
 ## Unzip files to new directory
 lapply(flz, function(x){
   unzip(zipfile = x,
-        exdir = paste0(p, df, "/"),
+        exdir = df,
         overwrite = FALSE)
 })
 
@@ -34,7 +36,7 @@ lapply(flz, function(x){
 # Combining datasets #
 
 ## Get list of all ACCESS files
-fl <- dir_ls(path = paste0(p, df, "/"),
+fl <- dir_ls(path = df,
              glob = "*.mdb")
 
 
@@ -44,7 +46,7 @@ fl <- dir_ls(path = paste0(p, df, "/"),
 dataLoc <- lapply(fl, function(x){
   cat("Processing locations: ", x, "\n")
   con = odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=",x))
-  data = sqlFetch(con, "Diag")
+  data = sqlFetch(con, "diag")
   data = select(data, -which(as.character(sapply(data, class)) %in% "ODBC_binary")) |>  # remove unrecognized ODBC_binary variables
      mutate( 
       across( # Correcting decimal degrees that have comma separator
@@ -68,6 +70,11 @@ dataLoc <- lapply(fl, function(x){
 }) |> 
   bind_rows()
 
+# Save dataset
+write_delim(x = dataLoc,
+            file = here::here("Consolidated_datasets", "loc_all_ses_raw_pre-qc.txt"),
+            delim = "\t"
+            )
 
 # ----------------------------------------------------------------------------------------------------------------------------------- #
 ## Create dive dataset
@@ -76,14 +83,14 @@ dataDive <- lapply(fl, function(x){
   con = odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=",x))
   
   data = tryCatch( # Catch any errors coming from their being no dive data
-    sqlFetch(con, "Dive"),
+    sqlFetch(con, "dive"),
     error = function(e){
-      message(paste("Dive data do not exist for", str_split(x, "/")[[1]][length(str_split(x, "/")[[1]])]))
+      message(paste("Unable to find dive data for", str_split(x, "/")[[1]][length(str_split(x, "/")[[1]])]))
     })
   
 if(is.null(data)){
-  return(data.frame(REF = str_split(x, "/")[[1]][length(str_split(x, "/")[[1]])]))
   odbcClose(con) # close ACCESS db connection
+  return(data.frame(REF = str_split(x, "/")[[1]][length(str_split(x, "/")[[1]])]))
 }else{  
   data = select(data, -which(as.character(sapply(data, class)) %in% "ODBC_binary")) |>  # remove unrecognised ODBC_binary variables
     mutate( 
@@ -102,9 +109,16 @@ if(is.null(data)){
       )
     ) |> 
     rename_with(.cols = everything(), .fn = toupper) # Change all colnames to uppercase
-  return(data)
   odbcClose(con) # close ACCESS db connection
+  return(data)
+  
 }
 }) |>  
   bind_rows()
+
+# Save dataset
+write_delim(x = dataDive,
+            file = here::here("Consolidated_datasets", "dive_all_ses_raw_pre-qc.txt"),
+            delim = "\t"
+)
 # ----------------------------------------------------------------------------------------------------------------------------------- #
